c0ea70b36365e1ab7b12b657375bdcba7be166f9
Matheus Minzon <125033723+CryptoMith@users.noreply.github.com>
2023-06-16 15:51:18 +0000

    Implementação do TypeScript da calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,7 +10,67 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  exibir(numero: number) {
-		console.log(numero);
-	}
+  valor1: any = null;
+  valor2: any = null;
+  resultado: any = null;
+  operador: any = null;
+  // variáveis que vão ser usados no código
+
+  SalvarValor(numero: any) {
+    // função que pega os números da calculadora (0-9)
+    if(this.valor1 == null) {
+      this.valor1 = numero;
+      this.resultado = numero;
+      // guarda o valor1 que antes era null e passa a ser o número que o usuário coloca
+    } else {
+      if(this.valor2 == null) {
+        this.valor2 = numero;
+        this.resultado = numero
+        // guarda o valor2 que antes era null e passa a ser o número que o usuário coloca
+      }
+    }
+    console.log(this.valor1);
+    console.log(this.valor2);
+  }
+
+  operacao(operador: any) {
+    this.operador = operador
+  }
+
+  calcular() {
+    // função que usa os operadores da calculadora (+, -, *, / e %)
+    if(this.operador == '+') {
+      this.resultado = this.valor1 + this.valor2;
+    } else if(this.operador == '-') {
+      this.resultado = this.valor1 - this.valor2;
+    } else if(this.operador == '*') {
+      this.resultado = this.valor1 * this.valor2;
+    } else if(this.operador == '/') {
+      this.resultado = this.valor1 / this.valor2;
+    } else if(this.operador == '%') {
+      this.resultado = this.valor1 % this.valor2;
+    }
+    // o operador pega o valor1 e valor2 e faz o cálculo com os operadores acima
+  }
+
+  LimparConta() {
+    this.valor1 = null;
+    this.valor2 = null;
+    this.resultado = null;
+
+    // função que transforma todos as variáveis que estavam com valor em variáveis com null 
+  }
+
+  Negativo() {
+    // função que transforma o número do positivo para negativo
+    if(this.valor1 != null && this.valor2 == null) {
+      this.valor1 = this.valor1 * (-1);
+      console.log(this.valor1);
+    } 
+    
+    if(this.valor1 != null && this.valor2 != null) {
+      this.valor2 = this.valor2 * (-1);
+      console.log(this.valor2); 
+    } 
+  }
 }
